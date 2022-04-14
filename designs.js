fee1a487386aeda58e96a5fe163bd5a57661fbf5
Princeton Rose <princetonjrose@gmail.com>
2022-04-13 23:07:13 -0400

    Finished JavaScript code! Yay!!! ^_^

--- designs.js
+++ designs.js
@@ -1,10 +1,45 @@
 // Select color input
+let color
+document.getElementById('colorPicker').onchange = (e)=> {
+    color = e.target.value
+    console.log(color)
+}
+
 // Select size input
+let size = document.getElementById('sizePicker')
 
 // When size is submitted by the user, call makeGrid()
+size.onsubmit = (e)=> {
+    e.preventDefault()
+    let height = size.height.value
+    let width = size.width.value
+    makeGrid( height, width )
+}
 
-function makeGrid() {
+let pixelCanvas = document.getElementById('pixelCanvas')
 
-// Your code goes here!
+let clearNode =( node )=> {
+    while ( node.firstChild )
+        node.firstChild.remove()
+}
+
+function makeGrid( height, width ) {
+
+    // Your code goes here!
+    clearNode( pixelCanvas )
+
+    for ( let h = 0; h < height; h++ ) {
+        let row = document.createElement('tr')
+        pixelCanvas.appendChild(row)
+        for ( let w = 0; w < width; w++ ) {
+            let block = document.createElement('td')
+            
+            block.onclick = (e)=> {
+                block.style.backgroundColor = color
+            }
+            
+            row.appendChild(block)
+        }
+    }
 
 }
